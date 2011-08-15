10fbec8fb131722b31e55527ddf786471d8d14c3
Joe Jenkinson <jj9854@bris.ac.uk>
2011-08-15 15:43:06 +0100

    Cleaned up code, returned input to const's

--- src/c/kernels/addBuffer.cl
+++ src/c/kernels/addBuffer.cl
@@ -1,10 +1,10 @@
 __kernel void sum(
-   __global float* input_1,
-   __global float* input_2,
+   __global const float* input_1,
+   __global const float* input_2,
    __global float* output
 )
 {
-	unsigned int gid = get_global_id(0);
-	output[gid] = input_1[gid] ;
+	int gid = get_global_id(0);
+	output[gid] = input_1[gid]+input_2[gid] ;
 
 }
--- src/python/guppy/kernels/AddBuffer.py
+++ src/python/guppy/kernels/AddBuffer.py
@@ -15,63 +15,28 @@ class AddBuffer(Kernel):
         Add up two 5-element arrays
         """
         mf = cl.mem_flags
-
         input_arr = numpy.array(range(0,10), dtype=numpy.float32)
 	i_two = numpy.array(range(10,0,-1), dtype=input_arr.dtype)
 
-
-       # parallelism = numpy.long(1)
-       # self.buffers.append((parallelism,))
-
         self.dtype = input_arr.dtype
         self.global_size = input_arr.shape
-      	self.local_size = self.global_size
 
-
+	#Create the buffers and append to a list
 	input_one = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=input_arr)
-        
 	self.buffers.append(input_one)
-
         input_two = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=i_two)
         self.buffers.append(input_two)
-	
 	out_buff = cl.Buffer(self.ctx, mf.WRITE_ONLY,size=input_arr.nbytes)
         self.buffers.append(out_buff)
-
-	print "called in Create Buffers"
-	print self.global_size
-	print self.dtype
-	print self.buffers[0].get_host_array(shape=self.global_size, dtype=self.dtype)
-	print self.buffers[1].get_host_array(shape=self.global_size, dtype=self.dtype)
-	print self.buffers[2].get_host_array(shape=self.global_size, dtype=self.dtype)
 
     def _run_kernel(self):
         """
         Run the kernel (self.program).
         """
-        # This is broken
-        print "These are the parameters that are sent:"
-        print self.global_size
-        print self.dtype
-        print self.buffers[0].get_host_array(shape=self.global_size, dtype=self.dtype)
-        print self.buffers[1].get_host_array(shape=self.global_size, dtype=self.dtype)
-        print self.buffers[2].get_host_array(shape=self.global_size, dtype=self.dtype)
-
 	self.program.sum(self.queue, self.global_size, None, *self.buffers)
 	print "these are read back"
         # Make an empty array to copy data into
-	a = numpy.zeros(self.global_size, dtype=self.dtype)   
-	b = numpy.zeros(self.global_size, dtype=self.dtype)
-	c = numpy.zeros(self.global_size, dtype=self.dtype)
-	print c
+	result_arr = numpy.empty(self.global_size, dtype=self.dtype)
      # Enqueue the copy
-  	cl.enqueue_copy(self.queue, a, self.buffers[0])
-	cl.enqueue_copy(self.queue, b,self.buffers[1])
-      	cl.enqueue_copy(self.queue, c,self.buffers[2])
-
-	print self.global_size
-        print self.dtype
-	print a
-	print b
-	print c
-	#return c
+  	cl.enqueue_copy(self.queue, result_arr, self.buffers[-1])
+	return result_arr
