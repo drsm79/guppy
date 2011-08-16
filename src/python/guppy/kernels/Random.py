9ab028e34620f640677d686a529d0d2a55587b15
Joe Jenkinson <jj9854@bris.ac.uk>
2011-08-16 12:43:37 +0100

    Random number generation working, to create n random numbers across x threads. Use of buffers to hold single values works okay but could be improved.

--- src/python/guppy/kernels/Random.py
+++ src/python/guppy/kernels/Random.py
@@ -3,6 +3,7 @@
 import pyopencl as cl
 from guppy.kernels import Kernel
 import numpy
+import math
 
 class Random(Kernel):
     """
@@ -10,7 +11,7 @@ class Random(Kernel):
     """
     kernel_file = "oclRandomKernel.cl"
 
-    def _create_buffers(self, size, parallelism=1, seeds=None):
+    def _create_buffers(self, size, threadcount, parallelism=1, seeds=None):
         """
         Define the necessary input and ouptut Buffer objects for the Kernel,
         store in self.input_buffers and self.output_buffers. Subclasses should
@@ -24,44 +25,55 @@ class Random(Kernel):
         #       d_seed = four random numbers in an array
         #       nPerRNG = number of random numbers to generate per thread
         #       RNG_COUNT = number of threads used
+	#	Target = the number of samples requested.
+	#
+	#	Example call:  rand(threadcount=256,size=777777)
+	#	requests 777777 random numbers to be generated over 256 threads.
+	#	777777 is clearly not a multiple of 256 so we say that nPerRNG ceil(777777.0/256.0) = 3039
+	#	i.e. each of the 256 thread generates 3038 numbers each, giving a total of 3039*256= 777984, 
+	#	and the kernels break their loops when the required 777777 have been calculated. This is why
+	#	'nPerRNG' (for-loop variable), RNG_COUNT (ie. num of threads, required to index the output)
+	# 	and Target (required size, to determine when the kernel can terminate early)
 
-        self.parallelism = numpy.long(parallelism)
-        #self.buffers.append((self.parallelism,))
-        self.global_size = (size,)
 
-        output_size = numpy.float32(size).nbytes * size * 20
-        self.buffers.append(cl.Buffer(self.ctx, mf.WRITE_ONLY, output_size))
+	#Calculate kernel variables
+	nPerRNG = math.ceil(float(size)/float(threadcount))
+        self.global_size = (threadcount,)
+	self.size = size
 
+	#set the writeable output array to hold the random numbers
+        output_size = numpy.float32(size).nbytes * size
+        self.buffers.append(cl.Buffer(self.ctx, mf.WRITE_ONLY, size=output_size))
+
+	#Generate seeds, required to be >180 by HybridTaus algorithm 
         if not seeds:
-            self.seeds = numpy.random.rand(size * 4).astype(numpy.float32)
+            self.seeds = numpy.random.randint(180, 1000000, (threadcount, 4)).astype(numpy.uint32)
         else:
             self.seeds = seeds
         seed_buffer = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.seeds)
         self.buffers.append(seed_buffer)
 
-        # TODO: check why an array is needed (something about buffer...) and look at alternatives
-        n_arr = numpy.array(numpy.long(size/parallelism))
+	#write nPerRNG to buffer as nPerRNG
+        n_arr = numpy.array(numpy.int(nPerRNG))
         n_bufr = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=n_arr)
         self.buffers.append(n_bufr)
 
-        p_arr = numpy.array(numpy.long(parallelism))
-        p_bufr = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=p_arr)
-        self.buffers.append(p_bufr)
+	#write threadcount to buffer as RNG_COUNT
+        tc_arr = numpy.array(numpy.int(threadcount))
+        tc_bufr = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=tc_arr)
+        self.buffers.append(tc_bufr)
 
-    #
-    # def _build_program(self):
-    #     TODO: set compile parameters to use include files, if necessary
-    #   self.program = cl.Program(self.ctx, self.kernel_string).build(options=[])
-    #
+	#Write size to buffer as TARGET
+	t_arr = numpy.array(numpy.int(size))
+	t_bufr = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=t_arr)
+	self.buffers.append(t_bufr)
 
     def _run_kernel(self):
         """
         Run the kernel (self.program). Subclasses should overwrite this.
         """
-        # This is broken
-        self.program.GPUrand(self.queue, self.global_size, None, *self.buffers, g_times_l=True)
-        # print len(self.buffers)
-        # print self.buffers[1].hostbuf
-        # print self.buffers[1].get_info()
-        # print self.buffers[1].get_host_array()
-        # return self.buffers[1].get_host_array()
+        self.program.GPUrand(self.queue, self.global_size, None, *self.buffers)
+	#Create empty array to output the random numbers result from device-->host
+	result_arr = numpy.empty(self.size, dtype=numpy.float32)
+	cl.enqueue_copy(self.queue, result_arr, self.buffers[0])
+	return result_arr
--- test/python/guppy_t/kernels_t/Random_t.py
+++ test/python/guppy_t/kernels_t/Random_t.py
@@ -6,12 +6,14 @@ from guppy.kernels.Random import Random
 import numpy
 from guppy_t import TimedTest
 
+
 class Random_t(unittest.TestCase):
     def testCall(self):
-        n_rands = 10
+        n_rands = 777777
+	n_threads = 256
         rand = Random()
-        result = rand(size=n_rands)
-
+        result = rand(threadcount=n_threads,size=n_rands)
+	print result
         self.assertEquals(n_rands, result.size)
 
 
